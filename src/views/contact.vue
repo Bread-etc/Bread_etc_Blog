35688152448885848922f784f2673ba06d09b149
Bread-etc <1054115624@qq.com>
2023-10-03 21:08:59 +0800

    2023/10/03 第五十七次提交

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -7,7 +7,8 @@
             <div :class="$style.textContent">
               <h1>欢迎来到我的留言板！👋</h1>
               <h2>如何留言？📝</h2>
-              <span>👉 <a href="https://github.com/login/oauth/authorize?client_id=983b9d8960cacfb8d0bc&state=breadeTc">Github</a> 登录后,在下方的留言框中输入您的留言内容即可。</span>
+              <span>经👉 <div>Github</div> 登录后,在下方的留言框中输入您的留言内容即可。</span>
+              <span>评论支持 <div>markdown</div> 格式和 <div>emoji😘</div></span>
               <span>请注意，为了保持良好的阅读体验喝交流氛围，请勿发布任何不良信息和言论。</span>
               <h2>免责声明 🧷</h2>
               <span>⚠️本站内容仅代表个人观点，可以转载，但请注明出处。</span>
@@ -17,30 +18,21 @@
             </div>
             <div :class="$style.divider"></div>
             <div :class="$style.messageContent">
-              <h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="currentColor" d="M17.74 30L16 29l4-7h6a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h9v2H6a4 4 0 0 1-4-4V8a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v12a4 4 0 0 1-4 4h-4.84Z"/><path fill="currentColor" d="M8 10h16v2H8zm0 6h10v2H8z"/></svg>
-                评论
-              </h2>
-              <div id="gitalk-container"  ></div>
               <div :class="$style.message">
-                <el-input
-                  v-model="textarea"
-                  :autosize="{ minRows: 2, maxRows: 4}"
-                  type="textarea"
-                  placeholder="请输入"
-                  maxlength="150"
-                  show-word-limit
+                <Giscus
+                  id="comments"
+                  repo="Bread-etc/Bread_etc_Blog"
+                  repoId="R_kgDOKDzLlw"
+                  category="Comments"
+                  categoryId="DIC_kwDOKDzLl84CZz5k"
+                  mapping="pathname"
+                  reactionsEnabled="1"
+                  emitMetadata="0"
+                  inputPosition="top"
+                  :theme="mode"
+                  lang="zh-CN"
+                  loading="lazy"
                 />
-                <button :class="$style.sendMessage" @click="sendMessage">
-                  发送
-                </button>
-              </div>
-              <div :class="$style.divider"></div>
-              <div :class="$style.messageList">
-                <h2>xx条评论</h2>
-                <div :class="$style.simpleMessage">
-                  132456
-                </div>
               </div>
             </div>
           </div>
@@ -50,35 +42,12 @@
 </template>
 
 <script lang="ts" setup>
-import { ElMessage } from 'element-plus';
-import 'gitalk/dist/gitalk.css';
-import Gitalk from 'gitalk';
-import { ref } from 'vue';
+import Giscus from '@giscus/vue'
+import { ref } from 'vue'
 
-const textarea = ref('')
-var gitalk = new Gitalk({
-  clientID: '983b9d8960cacfb8d0bc',
-  clientSecret: 'de468bc5a19b5fcfee8bddc946c657eee0f5cc71',
-  repo: 'blog-comments',
-  owner: 'Bread-etc',
-  admin: ['Bread-etc'],
-  id: location.pathname,
-  distractionFreeMode: false
-})
+const theme = ref(JSON.parse(localStorage.getItem('theme')))
+const mode = ref(theme.value ? 'dark' : 'light')
 
-gitalk.render('gitalk-container')
-
-// 方法
-const sendMessage = () => {
-  ElMessage({
-    showClose: false,
-    message: '发送评论成功！',
-    type: 'success',
-    center: true,
-
-  })
-}
- 
 </script>
 
 <style lang="scss" module>
